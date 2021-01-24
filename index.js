cbe98d4a49063a9f62421dcfb0aa0b79871624df
takamoja <mojage_and_flower@me.com>
2021-01-24 21:01:59 +0900

    add: http server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
