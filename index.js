90feeececb59c5835a5ab5f0a26ab641e70ae2a6
takamoja <mojage_and_flower@me.com>
2021-01-24 21:10:22 +0900

    fix: express settings

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+    .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
